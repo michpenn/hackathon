742108de359a3b87bfafe52acf3ef2dfb51dfebe
[ThinkPad]Ryan Transfiguracion <rtransfig@gmail.com>
2015-11-11 12:17:08 -0800

    Renamed ryan.html and ryan.js to index.html and main.js, respectively

--- apis_youtube.js
+++ apis_youtube.js
@@ -60,60 +60,6 @@ function YouTubeAPI_Skeleton()
      */
 }
 
-/**
- * COPY AND PASTE this skeleton function when you make a new YouTube API
- * DO NOT fill in the blanks for this skeleton
- *
- * @constructor - You are basically creating a object
- *
- *   Example to create an object with these properties, type:
- *   var my_youtube_api_skeleton = new YouTubeAPI_Skeleton();
- */
-function YouTubeAPI_Skeleton()
-{
-    /**
-     * Enter the URL for this API
-     * @type - string URL
-     */
-    this.api_url = '';
-
-
-
-    /**
-     * Enter the data object properties below.
-     * These are the key/value parameters you pass like in Postman.
-     * @type - object
-     */
-    this.api_data = {};
-
-
-    /**
-     * api_success
-     * Enter the stuff you want to do when the API returns a successful response.
-     * @param result - The object that is returned from the response (typically a JSON object)
-     */
-    this.api_success = function(result)
-    {
-
-    };
-
-
-    /**
-     * api_error
-     * Enter the stuff you want to do when the API returns a failed response.
-     * @param result - The object that is returned from the response (typically a JSON object)
-     */
-    this.api_error = function(result)
-    {
-
-    };
-
-
-    /**
-     * Add your own helper functions below.
-     */
-}
-
 
 function YouTubeAPI_Search()
 {
